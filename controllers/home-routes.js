eedd3eebb8a7edbee57e5866fe8cac6566edbb9b
Shital Chaudhary <shitalchau10@gmail.com>
2022-09-03 17:15:17 -0500

    create each models

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const {
+    User
+} = require('../models');
 
 router.get('/', (req, res) => {
+    res.send("Home Route");
     console.log(req.body);
-    res.send("hello");
-}); 
+});
 
 
-module.exports = router; 
+module.exports = router;
