e2c7f7c78f878f20a0e21923eb6418839129e05c
Shital Chaudhary <shitalchau10@gmail.com>
2022-09-07 20:06:09 -0500

    create layouts for main component

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,50 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const {
-    User
+    User, Post, Comment
 } = require('../models');
 
+// router.get('/', (req, res) => {
+//     res.send("Home Route");
+//     console.log(req.body);
+// });
+
+
 router.get('/', (req, res) => {
-    res.send("Home Route");
-    console.log(req.body);
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+              include: {
+                model: User,
+                attributes: ['username']
+              }
+            },
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+    })
+    .then(dbPostData => {
+        // pass a single post object into the homepage template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', {
+          posts
+        //   loggedIn: req.session.loggedIn
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 
